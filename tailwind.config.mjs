cfca64533b7ac3b88760ae09cec95edbe65f7ccf
Devin Minnihan <devinminnihan@gmail.com>
2025-04-22 18:11:19 -0400

    fix: add tailwind config

---
+++ tailwind.config.mjs
@@ -0,0 +1,42 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+const {
+  default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
+    },
+  },
+  plugins: [addVariablesForColors, addUtilitiesForScrollbar],
+};
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme('colors'));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ':root': newVars,
+  });
+}
+
+function addUtilitiesForScrollbar({ addUtilities }) {
+  addUtilities({
+    '.scrollbar-hide': {
+      '-ms-overflow-style': 'none' /* IE and Edge */,
+      'scrollbar-width': 'none' /* Firefox */,
+    },
+    '.scrollbar-hide::-webkit-scrollbar': {
+      display: 'none' /* Chrome, Safari, and Opera */,
+    },
+  });
+}
